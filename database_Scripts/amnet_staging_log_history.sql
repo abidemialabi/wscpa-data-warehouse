75b01422042c3c5e85480be98d7e1e87a8f0d68f
aeonbysagar <ssoni@affirma.com>
2026-08-19 18:31:35 +0530

    Add AM.Net Log History staging table

---
+++ database_Scripts/amnet_staging_log_history.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS wscpa_amnet.staging_log_history;
+
+CREATE TABLE wscpa_amnet.staging_log_history (
+    contacts_key INT,
+    log_entry_types_key INT,
+    add_dates_key CHAR(25),
+    follow_up_dates_key CHAR(25),
+    contact_id VARCHAR(20),
+    log_entry_note VARCHAR(128),
+    log_entry_memo VARCHAR(2000),
+    load_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
