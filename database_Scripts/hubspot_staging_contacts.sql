74f5c8ace51161ee3f4846a5a50984ed3a85ef94
aeonbysagar <ssoni@affirma.com>
2026-08-24 16:58:13 +0530

    Add HubSpot Contacts staging table

--- database_Scripts/hubspot_staging_contacts.sql
+++ database_Scripts/hubspot_staging_contacts.sql
@@ -1,6 +1,7 @@
-DROP TABLE IF EXISTS wscpa_breezio.staging_users;
+DROP TABLE IF EXISTS wscpa_hubspot.staging_contacts;
 
-
-CREATE TABLE wscpa_breezio.staging_users (
-    item LONGTEXT
+CREATE TABLE wscpa_hubspot.staging_contacts (
+    json_data LONGTEXT,
+    source_file VARCHAR(500),
+    load_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
