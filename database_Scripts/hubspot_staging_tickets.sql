2bbe328596547a6f86ece3a0c4adfe0bf6673554
aeonbysagar <ssoni@affirma.com>
2026-08-24 17:01:06 +0530

    Add HubSpot Tickets staging table

---
+++ database_Scripts/hubspot_staging_tickets.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS wscpa_hubspot.staging_tickets;
+
+CREATE TABLE wscpa_hubspot.staging_tickets (
+    json_data LONGTEXT,
+    source_file VARCHAR(500),
+    load_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
